64076da31538aec8972b8735839144c65fa8d23e
laysoon <163278347+laysoonchris@users.noreply.github.com>
2025-06-20 14:15:07 +0200

    Update Program.cs
    usunęłam zakomentowanie builder.Services.AddScoped<IDbService, DbService>();
    przepraszam, że po kolowium

--- KolokwiumAPBD2_Poprawa/Program.cs
+++ KolokwiumAPBD2_Poprawa/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DatabaseContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
-//builder.Services.AddScoped<IDbService, DbService>();
+builder.Services.AddScoped<IDbService, DbService>();
 
 var app = builder.Build();
 
